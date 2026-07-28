41290135d6b18431cc6c8da849bd12c1e2b6cb44
DonaldMcBlack <194431080+DonaldMcBlack@users.noreply.github.com>
2026-07-29 00:06:18 +0100

    Music Hook Rewrite
    + Tweaked PTV3_customsongs
    + Meatophobia now plays when in range of Pillar John
    + Music now ends when Pizza Time is over

--- src/Lua/Music.lua
+++ src/Lua/Music.lua
@@ -21,8 +21,6 @@ local jingle_blacklist = {
 }
 
 local function playPizzaTimeMusic()
-	if not PTV3:isPTV3() then return end
-	if not (displayplayer and displayplayer.PTRound) then return end
 	if gametype ~= GT_PTV3DM and not PTV3.pizzatime then return end
 	if PTV3.has_titlecard and leveltime < PTV3.maxTitlecardTime then return end
 
@@ -31,9 +29,13 @@ end
 
 local usesongs = CV_RegisterVar({
 	name = "PTV3_customsongs",
-	defaultvalue = "No",
-	flags = CV_NETVAR,
-	PossibleValue = CV_YesNo
+	defaultvalue = "Off",
+	flags = CV_SAVE|CV_CALL,
+	PossibleValue = CV_OnOff,
+	func = function(cv)
+		local string = cv.value == 0 and "off" or "on"
+		print("Custom lap songs are now "..string..".")
+	end
 })
 
 local function SwitchLapMusic(lap, list)
@@ -45,82 +47,62 @@ end
 addHook('PostThinkFrame', function()
 	if not PTV3:isPTV3() then return end
 
+	local p = (displayplayer.valid and displayplayer.PTRound) and displayplayer or nil
+
+	if not p then return end
+
 	local data = PTV3:__getsongdata()
-	local modsongs = data[skins[displayplayer and displayplayer.skin or "sonic"].name] or data["Default"]
+	local modsongs = data[skins[p and p.skin or "sonic"].name] or data["Default"]
 	local secretmusic = modsongs["Secret"] or "SECRET"
+	local loop = true
 
 	if PTV3.game_over <= PTV3.ranktransitiontime then
-		S_ChangeMusic(displayplayer.PTRound.specforce and "ERANK" or PTV3.ranks[displayplayer.PTRound.rank].music, false, displayplayer)
+		S_ChangeMusic(p.PTRound.specforce and "ERANK" or PTV3.ranks[p.PTRound.rank].music, false, p, nil, 0)
 		return
 	end
 
-	if displayplayer and displayplayer.PTRound and not playPizzaTimeMusic() then
-		if PTV3.has_titlecard and leveltime <= PTV3.maxTitlecardTime then
-
-			local mapTM = mapheaderinfo[gamemap].keywords.."TM"
-
-			if leveltime < PTV3.maxTitlecardTime
-			and mapmusname ~= mapTM then
-				mapmusname = mapTM
-				S_ChangeMusic(mapmusname, false)
-			end
+	if PTV3.has_titlecard and leveltime <= PTV3.maxTitlecardTime then
 		
-			if leveltime == PTV3.maxTitlecardTime
-			and mapmusname == mapTM then
-				mapmusname = mapheaderinfo[gamemap].musname
-				S_ChangeMusic(mapmusname, true)
-			end
+		local mapTM = mapheaderinfo[gamemap].keywords.."TM"
+		loop = leveltime == PTV3.maxTitlecardTime and true or false
+		mapmusname = leveltime < PTV3.maxTitlecardTime and mapTM or mapheaderinfo[gamemap].musname
+
+	elseif PTV3.has_titlecard and leveltime > PTV3.maxTitlecardTime or not PTV3.has_titlecard then
+		if not playPizzaTimeMusic() then
+			mapmusname = (p.PTRound.insecret and mapmusname ~= secretmusic) and secretmusic or (not p.PTRound.insecret and mapmusname == secretmusic) and mapheaderinfo[gamemap].musname or $
 		end
 
-		if displayplayer.PTRound.insecret and mapmusname ~= secretmusic then
-			mapmusname = secretmusic
-			S_ChangeMusic(mapmusname, true)
-		end
-
-		if not displayplayer.PTRound.insecret and mapmusname == secretmusic then
-			mapmusname = mapheaderinfo[gamemap].musname
-			S_ChangeMusic(mapmusname, true)
+		if PTV3.pillarJohn and PTV3.pillarJohn.valid then
+			local dist_from_john = R_PointToDist2(0, 0, R_PointToDist2(p.mo.x, p.mo.y, PTV3.pillarJohn.x, PTV3.pillarJohn.y), p.mo.z-PTV3.pillarJohn.z)
+			mapmusname = dist_from_john < 4000*FU and "MEATO" or mapheaderinfo[gamemap].musname
 		end
 	end
 
-	if not playPizzaTimeMusic() then return end
-	local loop = true
+	if PTV3.game_over == (21*TICRATE)-11 then
+		if PTV3.extreme then S_ChangeMusic("POTEND", false, p)
+		else S_StopMusic(p) end
+	end
 
-	if not (displayplayer and displayplayer.PTRound) then return end
-	local p = displayplayer
-	
+	if not playPizzaTimeMusic() then return S_ChangeMusic(mapmusname, loop) end
+
 	local song = nil
 
-
-	if #modsongs > 0 and usesongs.value then
-		song = SwitchLapMusic(p.PTRound.laps, modsongs)
-	else
-		song = SwitchLapMusic(p.PTRound.laps, music)
-	end
-
 	if gametype == GT_PTV3DM then
-		if not PTV3.has_titlecard
-		or leveltime > PTV3.maxTitlecardTime then
+		if not PTV3.has_titlecard or leveltime > PTV3.maxTitlecardTime then
 			song = "AOTKPS"
 		end
+	else
+		song = SwitchLapMusic(p.PTRound.laps, (#modsongs > 0 and usesongs.value) and modsongs or music)
 	end
 
 	if PTV3.extreme then
-		if PTV3.pizzaface and PTV3.pizzaface.skindata.extreme_theme then
-			song = PTV3.pizzaface.skindata.extreme_theme
-		else
-			song = "POTMAC"
-		end
+		song = PTV3.pizzaface and PTV3.pizzaface.skindata.extreme_theme or "POTMAC"
 	end
 
 	if PTV3.overtime then
-		local default
-		if PTV3.overtime_time > 15*TICRATE then default = "OVRTIA"
-		else default = "OVRTIB" end
-		
+		local default = PTV3.overtime_time > 15*TICRATE and "OVRTIA" or "OVRTIB"
 		song = modsongs["Overtime"] or default
 	end
-	
 
 	for _, jing in pairs(jingle_blacklist) do
 		if jing == S_MusicName(p) then S_ChangeMusic(mapmusname, loop, p, 0, muspos) break end
