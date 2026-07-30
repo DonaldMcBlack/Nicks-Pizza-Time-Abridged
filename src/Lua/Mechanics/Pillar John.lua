ff4afb0afe04d22c5ad882eb2372ce0c566373b6
DonaldMcBlack <194431080+DonaldMcBlack@users.noreply.github.com>
2026-07-30 18:47:00 +0100

    Pillar John Scared Sprites

--- src/Lua/Mechanics/Pillar John.lua
+++ src/Lua/Mechanics/Pillar John.lua
@@ -2,6 +2,7 @@ freeslot(
 	"MT_PTV3_PILLARJOHN",
 	"SPR_JOHN",
 	"S_PTV3_PILLARJOHN",
+	"S_PTV3_PILLARJOHNSCARED",
 	"sfx_jpilr"
 )
 
@@ -16,28 +17,35 @@ states[S_PTV3_PILLARJOHN] = {
 	nextstate = S_PTV3_PILLARJOHN
 }
 
+states[S_PTV3_PILLARJOHNSCARED] = {
+	sprite = SPR_JOHN,
+	frame = K|FF_ANIMATE,
+	tics = -1,
+	var1 = 4,
+	var2 = 2,
+	nextstate = S_PTV3_PILLARJOHNSCARED
+}
+
 mobjinfo[MT_PTV3_PILLARJOHN] = {
 	spawnstate = S_PTV3_PILLARJOHN,
+	spawnhealth = 2,
 	radius = 64*FU,
 	height = 200*FU,
 	flags = MF_SPECIAL|MF_ENEMY
 }
 
-addHook("ShouldDamage", function(t,i,s)
-	return true
-end, MT_PTV3_PILLARJOHN)
+addHook("ShouldDamage", function(t,i,s) return true end, MT_PTV3_PILLARJOHN)
+addHook('MobjDamage', function(t,i,s) return true end, MT_PTV3_PILLARJOHN)
 
-addHook('MobjDamage', function(t,i,s)
-	return true
-end, MT_PTV3_PILLARJOHN)
+addHook("MobjSpawn", function(john) PTV3.pillarJohn = john end, MT_PTV3_PILLARJOHN)
+addHook("MobjDeath", function(john, i, s) if not PTV3.pizzatime then PTV3:startPizzaTime(s.player, 1) end end, MT_PTV3_PILLARJOHN)
 
-addHook("MobjSpawn", function(john)
-	john.isAlive = true
-	PTV3.pillarJohn = john
-end, MT_PTV3_PILLARJOHN)
-
-local function killJohn(john, pmo)
+addHook("TouchSpecial", function(john, pmo)
+	if not (pmo and pmo.player and pmo.player.PTRound) then return true end
+	if john.health < 2 then return true end
 	if PTV3.pizzatime < 0 then return end
+
+	john.health = 1
 
 	local killAngle = R_PointToAngle2(john.x, john.y, pmo.x, pmo.y)
 
@@ -46,40 +54,33 @@ local function killJohn(john, pmo)
 	john.momz = 16*john.scale
 
 	S_StartSound(nil, sfx_jpilr)
-	john.isAlive = false
 	P_StartQuake(15*FU, 5*TICRATE)
 
-	if not PTV3.pizzatime then
-		PTV3:startPizzaTime(pmo.player, 1)
-	end
-end
+	if not PTV3.pizzatime then PTV3:startPizzaTime(pmo.player, 1) end
 
-addHook("TouchSpecial", function(john, pmo)
-	if not (pmo and pmo.player and pmo.player.PTRound) then return true end
-	if not john.isAlive then return true end
-
-	killJohn(john, pmo)
 	return true
-end, MT_PTV3_PILLARJOHN)
-
-addHook("MobjDeath", function(john, i, s)
-	if not (s and s.player and s.player.PTRound) then return end
-
-	if not PTV3.pizzatime then
-		PTV3:startPizzaTime(s.player, 1)
-	end
 end, MT_PTV3_PILLARJOHN)
 
 addHook("MobjThinker", function(john)
 	if not (john and john.valid) then return end
-
 	if PTV3.pizzatime < 0 then P_RemoveMobj(john) return end
 
-	if not john.isAlive then
+	if john.health >= 2 then
+		local can_see_player = P_LookForPlayers(john, 2000*FU, true, false)
+		if not can_see_player then john.target = nil end
+
+		if john.target ~= nil then
+			local p = john.target.player
+			local nextstate = p.pflags & (PF_SPINNING|PF_STARTDASH|PF_JUMPED) and S_PTV3_PILLARJOHNSCARED or S_PTV3_PILLARJOHN
+			if john.state ~= nextstate then john.state = nextstate end
+		elseif john.state ~= S_PTV3_PILLARJOHN then
+			john.state = S_PTV3_PILLARJOHN
+		end
+	else
 		john.flags = $|MF_NOCLIP|MF_NOCLIPHEIGHT
-		john.frame = K
-		if (john.z > john.ceilingz
-		or john.z+john.height < john.floorz) then
+		john.frame = P
+
+		if john.z > john.ceilingz or (john.z+john.height) < john.floorz then
 			P_RemoveMobj(john)
 		end
 	end
